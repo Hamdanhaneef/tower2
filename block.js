a2cbd9151db3d212dc68ce5ef938300205a76ce4
Hamdanhaneef <76527813+Hamdanhaneef@users.noreply.github.com>
2021-02-17 18:29:54 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,36 @@
+class Block{
+    constructor(x, y, width, height) {
+        var options = {
+           
+            restitution :0.4,
+            friction :1.0,
+            //isStatic:true
+           
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        var pos= this.body.position;
+        push();
+        if (this.body.speed < 3) {
+          rectMode(CENTER);
+          translate(pos.x, pos.y);
+          rotate(angle);
+          fill("black");
+          stroke(border);
+          strokeWeight(4);
+          rect(0, 0, this.width, this.height);
+          pop();
+      } else {
+          push();
+          World.remove(world, this.body);
+          this.visibility = this.visibility - 1;
+          tint(255, this.visibility);
+          pop();
+        
+      }
+}
